403257395717198f0427cbbf319a330c185ade2d
ashishngrr <nagarashish1806@gmail.com>
2024-04-09 22:47:10 +0530

    change in login route

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -7,7 +7,7 @@ const AdminAuthHelper = require("../helper/adminAuthHelper");
 let validateToken = AdminAuthHelper.validateToken; 
 
 router.post("/signup", AuthController.signup); 
-router.post("/signin", AuthController.signin); 
+router.post("/login", AuthController.signin); 
 router.delete("/delete/:id", validateToken, AuthController.deleteUser); 
 router.post("/updatePassword", validateToken, AuthController.updatePassword); 
 
