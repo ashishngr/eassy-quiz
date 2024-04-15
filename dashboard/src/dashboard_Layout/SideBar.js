070782cadf172a576902e1b59b4a935b3ddd0a43
ashishngrr <nagarashish1806@gmail.com>
2024-04-15 23:28:35 +0530

    change in dashboard sidebar layout

--- dashboard/src/dashboard_Layout/SideBar.js
+++ dashboard/src/dashboard_Layout/SideBar.js
@@ -10,35 +10,35 @@ import { CgProfile } from "react-icons/cg";
 
 const SideBar = () => {
   return (
-    <div className="fixed top-0 left-0 z-50 h-full w-64 overflow-y-auto bg-blue-50 text-gray-700 transition duration-300 ease-in-out border-gray-700">
+    <div className="fixed top-0 left-0 z-50 h-full w-64 overflow-y-auto bg-white  text-lg font-sans subpixel-antialiased transition duration-300 ease-in-out flex flex-col items-center border-solid border-r-4 border-light-blue-500">
       <div className="flex flex-col items-center py-4 px-6">
         <img className="w-20 h-20 mb-2" src={QuizLogo} alt="Your Platform Logo" /> 
         <h1 className="text-lg font-medium text-center"> {/* Username with styling */}
           Ashish Nagar
         </h1>
       </div>
-      <ul className="mt-6 space-y-2">
-        <li className="py-2 px-4 hover:bg-blue-100 flex items-center">
+      <ul className="mt-3 space-y-2 ">
+        <li className="w-52 py-2 px-3 flex items-center hover:bg-blue-800 hover:text-white hover:rounded-lg">
           {<MdHomeMax className='mr-2'/>}
-            <a href="/" className="flex items-center justify-center gap-2 text-md font-bold" >
+            <a href="/" className="flex items-center justify-center gap-2 text-sm " >
               Home
             </a>
         </li>
-        <li className="py-2 px-4 hover:bg-blue-100 flex items-center">
+        <li className="w-52 py-2 px-3 flex items-center hover:bg-blue-800 hover:text-white hover:rounded-lg">
           {<MdOutlineQuiz className='mr-2'/>}
-          <a href="/" className="flex items-center justify-center gap-2 text-md font-bold">
+          <a href="/" className="flex items-center justify-center gap-2 text-sm ">
              Quiz
           </a>
         </li>
-        <li className="py-2 px-4 hover:bg-blue-100 flex items-center">
+        <li className="w-52 py-2 px-3 flex items-center hover:bg-blue-800 hover:text-white hover:rounded-lg ">
         {<TbBrandGoogleAnalytics className='mr-2'/>}
-          <a href="/" className="flex items-center justify-center gap-2 text-md font-bold">
+          <a href="/" className="flex items-center justify-center gap-2 text-sm ">
            Analytic
           </a>
         </li>
-        <li className="py-2 px-4 hover:bg-blue-100 flex items-center">
+        <li className="w-52 py-2 px-3 flex items-center hover:bg-blue-800 hover:text-white hover:rounded-lg">
         {<CgProfile className='mr-2'/> }
-          <a href="/" className="flex items-center justify-center gap-2 text-md font-bold">
+          <a href="/" className="flex items-center justify-center gap-2 text-sm ">
           Profile
           </a>
         </li>
