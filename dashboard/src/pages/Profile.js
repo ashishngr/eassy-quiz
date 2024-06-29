43ab7faf59f5d36ff5c8cb13d93adedfa8a61bd8
ashishngrr <nagarashish1806@gmail.com>
2024-06-29 12:55:32 +0530

    change in user profile layout

--- dashboard/src/pages/Profile.js
+++ dashboard/src/pages/Profile.js
@@ -1,15 +1,15 @@
 import React, { useState } from 'react'
-import { TextField, Button, Container, Typography, Box, Grid } from '@mui/material';
+import { TextField, Button, Container, Typography, Box, Grid, Avatar, Divider } from '@mui/material';
 
 const Profile = () => {
   const [profile, setProfile] = useState({
     firstName: '',
     lastName: '',
     email: '',
-    address: '', 
     oldPassword: '',
     newPassword: '',
-    confirmNewPassword: ''
+    confirmNewPassword: '', 
+    profilePic: null, 
   });
   const handleChange = (e) => {
     const { name, value } = e.target;
@@ -26,8 +26,8 @@ const Profile = () => {
   };
 
   return (
-    <Container maxWidth="sm">
-      <Box
+    <Container maxWidth="lg">
+     <Box
         component="form"
         onSubmit={handleSubmit}
         sx={{
@@ -40,49 +40,74 @@ const Profile = () => {
         <Typography variant="h4" component="h1" gutterBottom>
           Profile
         </Typography>
-        <Grid container spacing={2}>
-          <Grid item xs={12} sm={6}>
-            <TextField
-              label="First Name"
-              variant="outlined"
-              name="firstName"
-              value={profile.firstName}
-              onChange={handleChange}
-              fullWidth
+        <Grid container spacing={2} alignItems="center">
+          <Grid item xs={12} sm={2}>
+            <Avatar
+              alt="Profile Picture"
+              src={profile.profilePic ? URL.createObjectURL(profile.profilePic) : ''}
+              sx={{ width: 100, height: 100 }}
             />
+            <input
+              type="file"
+              accept="image/*"
+              name="profilePic"
+              onChange={handleChange}
+              style={{ display: 'none' }}
+              id="profilePicInput"
+            />
+            <label htmlFor="profilePicInput">
+              <Button variant="outlined" component="span" sx={{ padding: '4px 8px', fontSize: '0.875rem', mt: 2 }}>
+                Update Profile 
+              </Button>
+            </label>
           </Grid>
-          <Grid item xs={12} sm={6}>
-            <TextField
-              label="Last Name"
-              variant="outlined"
-              name="lastName"
-              value={profile.lastName}
-              onChange={handleChange}
-              fullWidth
-            />
-          </Grid>
-          <Grid item xs={12} sm={6}>
-            <TextField
-              label="Email"
-              variant="outlined"
-              name="email"
-              value={profile.email}
-              onChange={handleChange}
-              type="email"
-              fullWidth
-            />
-          </Grid>
-          <Grid item xs={12} sm={6}>
-            <TextField
-              label="Address"
-              variant="outlined"
-              name="address"
-              value={profile.address}
-              onChange={handleChange}
-              fullWidth
-            />
+          <Grid item xs={12} sm={10}>
+            <Grid container spacing={2}>
+              <Grid item xs={12} sm={6}>
+                <TextField
+                  label="First Name"
+                  variant="outlined"
+                  name="firstName"
+                  value={profile.firstName}
+                  onChange={handleChange}
+                  fullWidth
+                />
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <TextField
+                  label="Last Name"
+                  variant="outlined"
+                  name="lastName"
+                  value={profile.lastName}
+                  onChange={handleChange}
+                  fullWidth
+                />
+              </Grid>
+              <Grid item xs={12} sm={12}>
+                <TextField
+                  label="Email"
+                  variant="outlined"
+                  name="email"
+                  value={profile.email}
+                  onChange={handleChange}
+                  type="email"
+                  fullWidth
+                />
+              </Grid>
+              <Grid item xs={12} sm={12}>
+                <Button
+                  variant="contained"
+                  color="primary"
+                  
+                  fullWidth
+                >
+                  Update Basic Information
+                </Button>
+              </Grid>
+            </Grid>
           </Grid>
         </Grid>
+        <Divider sx={{ my: 2 }} />
         <Typography variant="h5" component="h2" gutterBottom>
           Change Password
         </Typography>
@@ -113,7 +138,7 @@ const Profile = () => {
           type="password"
           fullWidth
         />
-        <Button type="submit" variant="contained" color="primary" fullWidth>
+        <Button type="submit" variant="contained" color="primary" fullWidth sx={{ mt: 2 }}>
           Save
         </Button>
       </Box>
